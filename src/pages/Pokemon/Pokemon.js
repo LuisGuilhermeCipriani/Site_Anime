fb9be7c056f209a42bbbfe2b8fd85d6c61408cdc
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-01-27 19:35:08 -0300

    Cria estrutura inicial das páginas

---
+++ src/pages/Pokemon/Pokemon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pokemon(){
+    return(
+        <div>
+            <h2>Página Pokemon</h2>
+        </div>
+    );
+}
+
+export default Pokemon;
---
+++ src/pages/Tipos/Tipos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tipos(){
+    return(
+        <div>
+            <h2>
+                Página Tipos
+            </h2>
+        </div>
+    );
+}
+
+export default Tipos;
