db2200e4a20b1009410bdaf353ca697801865605
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-01-27 22:58:27 -0300

    Consome api na página de tipos

--- src/pages/Pokemon/Pokemon.js
+++ src/pages/Pokemon/Pokemon.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Pokemon(){
     return(
         <div>
             <h2>Página Pokemon</h2>
+            <Link to='/'>Home</Link>
         </div>
     );
 }
--- src/pages/Tipos/Tipos.js
+++ src/pages/Tipos/Tipos.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Tipos(){
-    return(
+function Tipos() {
+
+    const [tipos, setTipos] = useState(null);
+
+    useEffect(() => {
+
+        function loadApi() {
+            let url = 'https://pokeapi.co/api/v2/type';
+            fetch(url)
+                .then((r) => r.json())
+                .then((json) => {
+                    setTipos(json);
+
+                })
+        }
+
+        loadApi();
+
+    }, []);
+
+
+    return (
         <div>
-            <h2>
-                Página Tipos
-            </h2>
+            <h2>Página Tipos</h2>
+            <div>
+                {tipos != null && tipos.results.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <strong>{item.name}</strong>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     );
 }
